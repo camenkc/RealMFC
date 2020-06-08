5cd72b6df22c0f72b652ca04e319d097ad1484b1
Cake_C <c390656112@163.com>
2020-06-08 21:32:54 +0800

    添加各种对话框类以及打开函数
    可以在某某Dlg.cpp/.h中对对话框进行修改了！

---
+++ CReaderLoginDlg.cpp
@@ -0,0 +1,34 @@
+﻿// CReaderLoginDlg.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "MFCTest03.h"
+#include "CReaderLoginDlg.h"
+#include "afxdialogex.h"
+
+
+// CReaderLoginDlg 对话框
+
+IMPLEMENT_DYNAMIC(CReaderLoginDlg, CDialogEx)
+
+CReaderLoginDlg::CReaderLoginDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DIALOGReaderLogin, pParent)
+{
+
+}
+
+CReaderLoginDlg::~CReaderLoginDlg()
+{
+}
+
+void CReaderLoginDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CReaderLoginDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CReaderLoginDlg 消息处理程序
---
+++ CReaderLoginDlg.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+
+#include "./DataIO/DataIO.h"
+#include "./MyControls/MyControls.h"
+// CReaderLoginDlg 对话框
+
+class CReaderLoginDlg : public CDialogEx
+{
+	DECLARE_DYNAMIC(CReaderLoginDlg)
+
+public:
+	CReaderLoginDlg(CWnd* pParent = nullptr);   // 标准构造函数
+	virtual ~CReaderLoginDlg();
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DIALOGReaderLogin };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+	DECLARE_MESSAGE_MAP()
+};
