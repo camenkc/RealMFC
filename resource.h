f48a6790814b160d28b70cd6b4a1af5c68fb4802
西行寺静 <65853669+SaigyoujiShizuka@users.noreply.github.com>
2020-06-08 13:40:43 +0800

    添加了读者信息管理菜单

--- resource.h
+++ resource.h
@@ -28,6 +28,11 @@
 #define ID_32771                        32771
 #define ID_32772                        32772
 #define ID_32773                        32773
+#define ID_32774                        32774
+#define ID_32775                        32775
+#define ID_32776                        32776
+#define ID_32777                        32777
+#define ID_32778                        32778
 #define IDM_MENU_MAIN                   58112
 
 // Next default values for new objects
@@ -35,7 +40,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        136
-#define _APS_NEXT_COMMAND_VALUE         32774
+#define _APS_NEXT_COMMAND_VALUE         32779
 #define _APS_NEXT_CONTROL_VALUE         1013
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
